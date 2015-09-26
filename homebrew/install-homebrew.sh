ba7d0757e9ee1a6be1baaadc9f67342d8738e897
laughingMan <isaac.hatton@gmail.com>
2015-09-26 14:32:23 -0600

    install homebrew and recepies or update everything

---
+++ homebrew/install-homebrew.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+  fi
+else
+  brew update
+  brew upgrade
+fi
+
+
+# Install homebrew packages
+
+if test ! $(which thefuck)
+then
+  brew install thefuck
+fi
+
+if test ! $(which adb)
+then
+  brew install android-platform-tools
+fi
+
+exit 0
